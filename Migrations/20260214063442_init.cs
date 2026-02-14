7210c112539d5350447ef40071d96c08a06bb7f8
Max <max47911.eng@gmail.com>
2026-02-14 13:36:10 +0700

    migr

--- Migrations/20260214063442_init.cs
+++ Migrations/20260214063442_init.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace TodoList.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
