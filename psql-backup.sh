772a6c65a7c83cea8428d57b1525ee60798b8317
Max <max47911.eng@gmail.com>
2026-03-11 07:49:08 +0000

    new psql-backup.sh: SQL

--- psql-backup.sh
+++ psql-backup.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
+DATE=$(date +%F)
 BACKUP_DIR="/var/lib/postgresql/backups/"
 
-HOST="localhost"
-PORT="5432"
-DATABASE="todo_db"
-USERNAME="max"
-export PGPASSWORD="postgres"
+mkdir -p $BACKUP_DIR
 
-RETENTION_DAYS=7
-DATE=$(date +"%Y%m%d_%H%M%S")
-BACKUP_PATH="${BACKUP_DIR}/${DATE}"
-
-pg_basebackup -h ${HOST} -p ${PORT} -U ${USERNAME} -D ${BACKUP_PATH} --format=tar --gzip --wal-method=stream --progress --verbose
-
-
-
+sudo -u postgres pg_dump test_db > $BACKUP_DIR/testdb_$DATE.sql
